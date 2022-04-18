07da216de660c985d7416d29f9829409e34dbf52
gnorken <64843696+gnorken@users.noreply.github.com>
2022-04-18 17:52:46 +0200

    endringer

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-function changeColor(newColor) {
-    var elem = document.getElementById('container');
-    elem.style.color = newColor;
-  }
+const p = document.querySelector('p');
+p.onmouseover = logMouseOver;
+p.onmouseout = logMouseOut;
+
+function logMouseOver() {
+  p.textContent = 'MOUSE OVER detected';
+}
+
+function logMouseOut() {
+  p.textContent = 'MOUSE OUT detected';
+  setTimeout(() => p.textContent = "That was intense.", 1200);
+
+
+}
